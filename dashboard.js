47764c82cb04d8c4e11d4a01a70916e018ab3f9a
Kishore <kishoregirivasan11@gmail.com>
2023-06-12 10:06:31 +0530

    commit

---
+++ dashboard.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+  });
+  
