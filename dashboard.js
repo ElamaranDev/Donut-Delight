b4acfa1df8347b49d1c6432d7f5af591d5846039
ElamaranDev <marandev007@gmail.com>
2023-06-14 17:55:22 +0530

    menu responsive hamburger

--- dashboard.js
+++ dashboard.js
@@ -12,3 +12,24 @@ window.addEventListener('scroll', function() {
     }
   });
   
+  const hamburger = document.querySelector(".hamburger");
+  const initialHTML = hamburger.innerHTML;
+  
+  hamburger.addEventListener('click', function(){
+    let status = hamburger.getAttribute('data-status');  
+    if(status === 'not-active') {
+      hamburger.setAttribute('data-status','active');
+      hamburger.classList.add('hamburger-active');
+      console.log(hamburger.getAttribute('data-status'));
+      hamburger.innerHTML = `
+        <div id="1" class="line"></div>
+        <div id="2" class="line"></div>
+      `;
+    } else {
+      hamburger.setAttribute('data-status','not-active');
+      hamburger.classList.remove('hamburger-active');
+      console.log(hamburger.getAttribute('data-status'));
+      hamburger.innerHTML = initialHTML;
+    }
+  });
+  
