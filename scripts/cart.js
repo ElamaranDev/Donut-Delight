732432a68e3e4592476f9fb62a7d078d69740006
ElamaranDev <marandev007@gmail.com>
2023-06-20 19:07:19 +0530

    cart changes

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,9 +1,9 @@
-const cartItems = JSON.parse(localStorage.getItem('cart'));
+const cartItems = JSON.parse(localStorage.getItem("cart"));
 renderCart();
 function renderCart() {
-    let cartHTML = '';
-    cartItems.forEach((cartItem, index) => {
-      cartHTML += `
+  let cartHTML = "";
+  cartItems.forEach((cartItem, index) => {
+    cartHTML += `
         <div class="cart-item">
             <img src="${cartItem.image}" alt="cart-item-image">
             <div class="product-info">
@@ -19,65 +19,69 @@ function renderCart() {
                 </div>
             </div>
         </div>`;
-    });
-    document.querySelector('.js-cart-items').innerHTML = cartHTML;
-  
-    // Attach event listeners to update buttons
-    const updateButtons = document.querySelectorAll('.update-btn');
-    updateButtons.forEach((updateBtn, index) => {
-    updateBtn.addEventListener('click', () => {
-    const quantityInput = updateBtn.parentNode.querySelector('.update');
-    if (updateBtn.innerHTML === 'Update') 
-    {
-        quantityInput.disabled = false;
-        updateBtn.innerHTML = 'Save';
-        quantityInput.classList.add('update-clicked');
-    } 
-    else 
-    {
-        quantityInput.disabled = true;
-        updateBtn.innerHTML = 'Update';
-        quantityInput.classList.remove('update-clicked');
-        const updatedQuantity = parseInt(quantityInput.value);
-        cartItems[index].quantity = updatedQuantity;
-        localStorage.setItem('cart', JSON.stringify(cartItems));
-    }
-    calculateCost();
   });
-});
-calculateCost();
-}
+  document.querySelector(".js-cart-items").innerHTML = cartHTML;
 
-function deleteCartItem(index){
-    cartItems.splice(index, 1);
-    renderCart();
-    calculateCost();
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-}
+  // Attach event listeners to update buttons
+  const updateButtons = document.querySelectorAll(".update-btn");
+  updateButtons.forEach((updateBtn, index) => {
+    updateBtn.addEventListener("click", () => {
+      const quantityInput = updateBtn.parentNode.querySelector(".update");
+      if (updateBtn.innerHTML === "Update") {
+        quantityInput.disabled = false;
+        updateBtn.innerHTML = "Save";
+        quantityInput.classList.add("update-clicked");
+      } else {
+        const updatedQuantity = parseInt(quantityInput.value);
+        if (updatedQuantity === 0 || updatedQuantity < 0) {
+          deleteCartItem(index);
+        } else {
+          cartItems[index].quantity = updatedQuantity;
+          localStorage.setItem("cart", JSON.stringify(cartItems));
+        }
+        quantityInput.disabled = true;
+        updateBtn.innerHTML = "Update";
+        quantityInput.classList.remove("update-clicked");
+      }
 
-function calculateCost(){
-    const itemsCount = document.querySelector('#item-count-lable');
-    const itemPrice = document.querySelector('#item-price');
-    const tax = document.querySelector('#tax-amount');
-    const total = document.querySelector('#total-amount');
-    const deliveryCharge = document.querySelector('#delivery-charge');
-    let Cost = 0;
-    let quantity = 0;
-    let totalCost = 0;
-    let taxAmount = 0;
-    cartItems.forEach((item, index) => {
-        const itemQuantity = parseInt(item.quantity);
-        const price = parseInt(item.price);
-        const itemCost = itemQuantity * price;
-        Cost += itemCost;
-        quantity += item.quantity;
+      calculateCost();
     });
-    taxAmount = Cost * 0.05;
-    totalCost = quantity > 0 ? (taxAmount + Cost + 40) : (taxAmount + Cost);
+  });
+  calculateCost();
+}
 
-    itemsCount.innerHTML = `items(${quantity})`;
-    itemPrice.innerHTML = `₹${Cost}`;
-    tax.innerHTML = `₹${Math.floor(taxAmount)}`;
-    total.innerHTML = `₹${Math.floor(totalCost)}`;
-    deliveryCharge.innerHTML = totalCost !== 0 ? `₹40` : `₹0`;
+function deleteCartItem(index) {
+  cartItems.splice(index, 1);
+  renderCart();
+  calculateCost();
+  localStorage.setItem("cart", JSON.stringify(cartItems));
+}
+
+function calculateCost() {
+  const itemsCount = document.querySelector("#item-count-lable");
+  const itemPrice = document.querySelector("#item-price");
+  const tax = document.querySelector("#tax-amount");
+  const total = document.querySelector("#total-amount");
+  const deliveryCharge = document.querySelector("#delivery-charge");
+  const checkoutLable = document.querySelector("#checkout-lable");
+  let Cost = 0;
+  let quantity = 0;
+  let totalCost = 0;
+  let taxAmount = 0;
+  cartItems.forEach((item, index) => {
+    const itemQuantity = parseInt(item.quantity);
+    const price = parseInt(item.price);
+    const itemCost = itemQuantity * price;
+    Cost += itemCost;
+    quantity += item.quantity;
+  });
+  taxAmount = Cost * 0.05;
+  totalCost = quantity > 0 ? taxAmount + Cost + 40 : taxAmount + Cost;
+
+  checkoutLable.innerHTML = `<Strong>Checkout (</Strong>${quantity} items<Strong>)</Strong>`;
+  itemsCount.innerHTML = `items(${quantity})`;
+  itemPrice.innerHTML = `₹${Cost}`;
+  tax.innerHTML = `₹${Math.floor(taxAmount)}`;
+  total.innerHTML = `₹${Math.floor(totalCost)}`;
+  deliveryCharge.innerHTML = totalCost !== 0 ? `₹40` : `₹0`;
 }
